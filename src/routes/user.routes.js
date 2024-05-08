e38376d39b81ee5d110734617351c5109a6ddd09
Khushi-mehta13 <143219598+Khushi-mehta13@users.noreply.github.com>
2024-05-08 15:13:09 +0530

    Add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { LoginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
+import { LoginUser, changeCurrentPaasword, getCurrentUser, getUserChannelProfile, getWatchHistory, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -19,9 +19,15 @@ router.route("/register").post(
 
 router.route("/login").post(LoginUser)
 
-
 //secured routes
 router.route("/logout").post(verfiyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verfiyJWT,changeCurrentPaasword)
+router.route("/current-user").get(verfiyJWT,getCurrentUser)
+router.route("/update-account-Details").patch(verfiyJWT,updateAccountDetails)
+router.route("/avatar").patch(verfiyJWT,upload.single("/avatar"),updateUserAvatar)
+router.route("/coverImage").patch(verfiyJWT,upload.single("/coverImage"),updateUserCoverImage)
+router.route("/channel/:username").get(verfiyJWT,getUserChannelProfile)
+router.route("/history").get(verfiyJWT,getWatchHistory)
 
 export default router
